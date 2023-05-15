24e678f2ed8585765629f5f07a885b749f9b6ae2
joseph lebiane <joseph.lebiane@outlook.com>
2023-05-15 13:55:53 -0700

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+import { logo } from "../assets/";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="Sumz Logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => {
+            window.open("");
+          }}
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GBT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, an open-source article summarizer
+        taht transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
